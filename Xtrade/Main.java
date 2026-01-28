487f51eb3b55ac40934f1e062e1fc6532fa27745
Reda-Benbaih <wareda145@gmail.com>
2026-01-28 17:51:34 +0100

    Update Main.java

--- Xtrade/Main.java
+++ Xtrade/Main.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scan = new Scanner(System.in);
+        Market newMarket = new Market();
+        newMarket.addAdmin();
+        while (true){
+            System.out.println("1 pour admin");
+            System.out.println("2 pour trader");
+            System.out.println("0 pour quitter");
+            int choice = scan.nextInt();
+            if(choice == 0 ){
+                System.out.println("programme terminee");
+                break;
+            }
+            switch (choice){
+                case 1 :
+                    Boolean check = newMarket.adminCheck();
+                    if(check == true ){
+                        newMarket.adminMenu();
+                    }
+                    break;
+                case 2 : newMarket.traderMenu();break;
+                default:
+                    System.out.println("ce choix n'existe pas !!");
+            }
         }
     }
 }
