1fc1984f80813799f8fa12ec5c37e3f4525e75c7
Reda-Benbaih <wareda145@gmail.com>
2026-01-28 17:53:07 +0100

    Update Trader.java

--- Xtrade/Trader.java
+++ Xtrade/Trader.java
@@ -1,19 +1,20 @@
-public class Trader extends Person{
-    private int traderId;
-    private double balence ;
+import java.util.ArrayList;
 
-    public Trader (String name,String id,int age,int traderId,double balence){
+public class Trader extends Person{
+    final private int traderId;
+    private double balence ;
+    private static int idCounter = 1;
+    private Portfolio<Double,Asset,Double> portfolio;
+
+    public Trader (String name,String id,int age,double balence){
         super(name,id,age);
-        this.traderId = traderId;
+        this.traderId = idCounter++;
         this.balence = balence;
+        this.portfolio = new Portfolio<>(balence, new ArrayList<>(), 0.0);
     }
 
     public int getTraderId() {
         return traderId;
-    }
-
-    public void setTraderId(int traderId) {
-        this.traderId = traderId;
     }
 
     public double getBalence() {
@@ -22,5 +23,13 @@ public class Trader extends Person{
 
     public void setBalence(double balence) {
         this.balence = balence;
+    }
+
+    public Portfolio<Double, Asset, Double> getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio<Double, Asset, Double> portfolio) {
+        this.portfolio = portfolio;
     }
 }
