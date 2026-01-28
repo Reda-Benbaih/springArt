234359fd00abf32c0408af964641413a5d13cc79
Reda-Benbaih <wareda145@gmail.com>
2026-01-28 17:52:37 +0100

    Update Portfolio.java

--- Xtrade/Portfolio.java
+++ Xtrade/Portfolio.java
@@ -1,3 +1,37 @@
-public class Portfolio {
+import java.util.List;
 
+public class Portfolio<B,A,AV> {
+    private B balence;
+    private List<A> assets;
+    private AV assetValue;
+
+    public Portfolio(B balence, List<A> assets,AV assetvalue) {
+        this.balence = balence;
+        this.assets = assets;
+        this.assetValue = assetvalue;
+    }
+
+    public B getBalence() {
+        return balence;
+    }
+
+    public void setBalence(B balence) {
+        this.balence = balence;
+    }
+
+    public List<A> getAssets() {
+        return assets;
+    }
+
+    public void setAssets(List<A> assets) {
+        this.assets = assets;
+    }
+
+    public AV getAssetvalue() {
+        return assetValue;
+    }
+
+    public void setAssetvalue(AV assetvalue) {
+        this.assetValue = assetvalue;
+    }
 }
